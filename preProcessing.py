e399727a92d726f6c9e896ea117d5996896a52b7
Leonardo <leomaben@hotmail.com>
2025-03-27 12:00:59 +0100

    Image Augmentation of source images

--- preProcessing.py
+++ preProcessing.py
@@ -2,6 +2,8 @@ import numpy as np
 import os
 import cv2 as cv
 from sklearn.model_selection import train_test_split
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
 
 def loadImages(folder_path:os.path, label:int):
@@ -16,6 +18,30 @@ def loadImages(folder_path:os.path, label:int):
             labels.append(label)
 
     return images, labels
+
+
+def imageAugementation(image, label):
+    image = tf.image.random_flip_left_right(image)
+    image = tf.image.random_flip_up_down(image)
+    image = tf.image.random_brightness(image, max_delta=0.2)
+    image = tf.image.random_contrast(image, lower=0.8, upper=1.2)
+    return image, label
+
+
+def checkAugmentedImages(tensor_dataset):
+    images, labels = next(iter(tensor_dataset))
+    images = np.array(images)
+    labels = np.array(labels)
+
+
+    plt.figure(figsize=(12, 6))
+    for i in range(5):
+        plt.subplot(1, 5, i + 1)
+        plt.imshow(images[i])
+        class_label = 'Non Defective' if labels[i] == 0 else 'Defective'
+        plt.xlabel(class_label)
+    plt.suptitle('Test Images')
+    plt.show()
 
 
 def main():
@@ -33,7 +59,22 @@ def main():
 
     X_train, X_test, y_train, y_test = train_test_split(images ,labels, test_size=0.2, stratify=labels, random_state=42)
 
-    print(f"Training samples: {len(X_train)}, Testing samples: {len(X_test)}")
+    # print(f"Training samples: {len(X_train)}, Testing samples: {len(X_test)}")
+
+    train_ds = tf.data.Dataset.from_tensor_slices((X_train, y_train))
+    test_ds = tf.data.Dataset.from_tensor_slices((X_test, y_test))
+
+    train_ds = train_ds.map(imageAugementation).batch(32).shuffle(100)
+    test_ds = test_ds.batch(32)
+
+    checkAugmentedImages(train_ds)
+
+    num_batches = sum(1 for _ in train_ds)
+    print(f"Total number of batches: {num_batches}")
+
+    num_images = sum(len(images) for images, _ in train_ds)
+    print(f"Total number of images: {num_images}")
+
 
 if __name__ == '__main__':
     main()
